d61434176f59578fe2d58720c5e13a41dc0c7f5c
Rafo-1999 <mn.raf.99@mail.ru>
2022-01-09 17:58:23 +0400

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello java");
         System.out.println("hello github");
+        System.out.println("hello copy");
     }
     }
 
